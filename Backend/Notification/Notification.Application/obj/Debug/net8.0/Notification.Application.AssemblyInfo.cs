74185d7535be675d15cf37666678816ec7f84705
FaiTh725 <sasha.zelenukho.2016@mail.ru>
2025-04-12 15:04:02 +0300

    -

--- Backend/Notification/Notification.Application/obj/Debug/net8.0/Notification.Application.AssemblyInfo.cs
+++ Backend/Notification/Notification.Application/obj/Debug/net8.0/Notification.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notification.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81b75012087dfad27c673d1ba834259e16c68f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3575ba9c580939f9b22dd666045f64b892876a01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notification.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notification.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
